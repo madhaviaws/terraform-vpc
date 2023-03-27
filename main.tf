f60d5c21ca247faf2e4cac60da19f4cb4176f794
madhaviaws <madhavi.oct14@gmail.com>
2023-03-27 13:24:16 +0530

    tf vpc

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ module "vpc" {
   source                = "./vendor/modules/vpc"
   VPC_CIDR              = var.VPC_CIDR
   ENV                   = var.ENV
-  PUBLIC_SUBNET_CIDR    = var.PUBLIC_SUBNET_CIDR
-  PRIVATE_SUBNET_CIDR   = var.PRIVATE_SUBNET_CIDR
+  #PUBLIC_SUBNET_CIDR    = var.PUBLIC_SUBNET_CIDR
+  #PRIVATE_SUBNET_CIDR   = var.PRIVATE_SUBNET_CIDR
 }
 
 # Always the source attribute in terraform module cannot be parameterized.
